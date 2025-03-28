e93c81df1ae3435db57b06179065bd67569c40a7
Idris Akintobi <akintobiidris@gmail.com>
2025-03-28 22:13:03 +0100

    added auth handler

---
+++ handlers/auth-handler.go
@@ -0,0 +1,43 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/IdrisAkintobi/go-basic-crud/handlers/dto"
+	"github.com/IdrisAkintobi/go-basic-crud/middlewares"
+	"github.com/IdrisAkintobi/go-basic-crud/services"
+	"github.com/IdrisAkintobi/go-basic-crud/utils"
+	"github.com/jackc/pgx/v5"
+)
+
+type AuthHandler struct {
+	as *services.AuthService
+}
+
+func NewAuthHandler(pg *pgx.Conn) *AuthHandler {
+	return &AuthHandler{as: services.NewAuthService(pg)}
+}
+
+func (ah *AuthHandler) Login(w http.ResponseWriter, r *http.Request) {
+	var loginData dto.AuthLoginReqDTO
+
+	err := json.NewDecoder(r.Body).Decode(&loginData)
+	if err != nil {
+		utils.SendErrorResponse(w, err.Error(), http.StatusBadRequest)
+	}
+
+	reqFingerprint := r.Context().Value(utils.FPCtxKey).(middlewares.UserFingerprint)
+
+	loginData.IPAddress = reqFingerprint.IPAddress
+	loginData.UserAgent = reqFingerprint.UserAgent
+
+	token, err := ah.as.SignIn(&loginData)
+	if err != nil {
+		utils.SendErrorResponse(w, err.Error(), http.StatusBadRequest)
+	}
+
+	resp := dto.AuthLoginResDTO{Token: token}
+
+	utils.SendSuccessResponse(w, resp, http.StatusCreated)
+}
---
+++ handlers/dto/auth-handler-dto.go
@@ -0,0 +1,12 @@
+package dto
+
+type AuthLoginReqDTO struct {
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+	UserAgent string `json:"userAgent"`
+	IPAddress string `json:"ipAddress"`
+}
+
+type AuthLoginResDTO struct {
+	Token string `token:"email"`
+}
